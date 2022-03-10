9305fb7cb1a4904b0c6ca10ba07c5a0c23df17ff
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-03-10 12:15:45 -0500

    somehing'

--- my_portfolio/src/components/About/About.js
+++ my_portfolio/src/components/About/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import "./style.css";
 
-function About () {
-    return (
-        <>
+function About() {
+  return (
+    <>
+      <div className="about-container">
+        <div className="img-container">
+          <img
+            className="img-esteban"
+            src="Images/esteban.jpeg"
+            alt="Esteban Mendez"
+          />
+        </div>
         <h1>ABout me</h1>
         <p>Some deep message that moves employers to hire me.</p>
-        </>
-    )
+      </div>
+    </>
+  );
 }
 
 export default About;
