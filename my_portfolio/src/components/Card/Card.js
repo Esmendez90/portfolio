083439fbbfc128277ef2766ec10a91cb839781a4
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-03-22 10:15:23 -0400

    added styling for progress bar and language names

--- my_portfolio/src/components/Card/Card.js
+++ my_portfolio/src/components/Card/Card.js
@@ -1,49 +1,59 @@
 import React from "react";
 import "./style.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
 import { Card, Button } from "react-bootstrap";
 import cardsProject from "./cardsProjects.json";
-import barItems from "./barItems.json";
-import { ListEl } from "../Reusable/Reusable";
 
 function CardComp() {
   return (
     <>
-      {cardsProject.map(({ id, image, alt, title, text }) => (
-        <Card key={id}>
-          <Card.Body>
-            <Card.Title>{title}</Card.Title>
-            <Card.Text>{text}</Card.Text>
-            <div>
-              <Card.Img variant="top" src={image} alt={alt} />
-            </div>
-            <Button variant="primary">Go somewhere</Button>
-            <div className="languages-bar">
-              <Card.Title>Languages</Card.Title>
-              <span className="progress">
-                <span className="progress-item js-bar"></span>
-                <span className="progress-item css-bar"></span>
-                <span className="progress-item html-bar"></span>
-              </span>
-              <ul className="lang-list-container">
-                {barItems.map(({ id, itemClass, href, target, language }) => (
-                  <div className="item-container" key={id}>
-                    <FontAwesomeIcon icon={faCircle} />
-                    <ListEl
-                      key={id}
-                      liClass={itemClass}
-                      href={href}
-                      target={target}
-                      children={language}
-                    />
-                  </div>
-                ))}
-              </ul>
-            </div>
-          </Card.Body>
-        </Card>
-      ))}
+      {cardsProject.map(
+        ({
+          id,
+          image,
+          alt,
+          title,
+          text,
+          js,
+          html,
+          css,
+          jsPercent,
+          htmlPercent,
+          cssPercent,
+        }) => (
+          <Card key={id}>
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{text}</Card.Text>
+              <div>
+                <Card.Img variant="top" src={image} alt={alt} />
+              </div>
+              <Button variant="primary">Go somewhere</Button>
+              <div className="languages-bar">
+                <Card.Title>Languages</Card.Title>
+                <span className="progress">
+                  <span
+                    className="progress-item js-bar"
+                    style={{ width: jsPercent }}
+                  ></span>
+                  <span
+                    className="progress-item html-bar"
+                    style={{ width: htmlPercent }}
+                  ></span>
+                  <span
+                    className="progress-item css-bar"
+                    style={{ width: cssPercent }}
+                  ></span>
+                </span>
+                <ul className="lang-list-container">
+                  <li className="item js-class">{js} <span>{jsPercent}</span></li>
+                  <li className="item html-class">{html} <span>{htmlPercent}</span></li>
+                  <li className="item css-class">{css} <span>{cssPercent}</span></li>
+                </ul>
+              </div>
+            </Card.Body>
+          </Card>
+        )
+      )}
     </>
   );
 }
