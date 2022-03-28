649cf7389642d8a4e739173f62771b43171e5f5e
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2022-03-28 15:51:12 -0400

    edited about page, rearranged components, edited styles

--- my_portfolio/src/components/About/About.js
+++ my_portfolio/src/components/About/About.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./style.css";
-import ImgTag  from "../Reusable/Reusable";
+import ImgTag from "../Reusable/Reusable";
 
 function About() {
   return (
     <>
       <div className="about-container">
+        <p>Hi there! My name is</p>
+        <h1 className="my-name">Esteban Mendez</h1>
+        <h2>... and I love designing webpages.</h2>
         <ImgTag
-          containerClass="img-container"
-          imgClass="img-esteban"
-          src={process.env.PUBLIC_URL + '/Images/esteban.jpeg'}
-          alt="Esteban Mendez"
-        />
-
-        <h1>Web Developer</h1>
+            containerClass="img-container"
+            imgClass="img-esteban"
+            src={process.env.PUBLIC_URL + "/Images/esteban.jpeg"}
+            alt="Esteban Mendez"
+          />
         <p>
           I am a front-end developer, familiar with back-end, knowledgeable of
           HTML, CSS, Javascript, Git/Github, jQuery, Bootstrap, node.js,
