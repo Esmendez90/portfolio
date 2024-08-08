9bd38af4d4f6812b177a32e16af9661d6a40f200
Esteban Mendez <estebanmendez@Estebans-MacBook-Pro.local>
2024-08-08 12:44:41 -0400

    Updates

---
+++ precache-manifest.6e84265eb6a78a5e3febaaaadf1a4f3c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eec0e2c1072cf3114297e25a4bbeba4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bc73599b34a9f3b4cd0d",
+    "url": "/portfolio/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "53b13e83b2a50243c271",
+    "url": "/portfolio/static/css/main.d12a9417.chunk.css"
+  },
+  {
+    "revision": "bc73599b34a9f3b4cd0d",
+    "url": "/portfolio/static/js/2.a5590435.chunk.js"
+  },
+  {
+    "revision": "9bb0e331dfb692c4abdaf9705161e76a",
+    "url": "/portfolio/static/js/2.a5590435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "263d7caba0fe0120a472",
+    "url": "/portfolio/static/js/3.8c38d570.chunk.js"
+  },
+  {
+    "revision": "53b13e83b2a50243c271",
+    "url": "/portfolio/static/js/main.d42cd7ca.chunk.js"
+  },
+  {
+    "revision": "641e499123529b177b03",
+    "url": "/portfolio/static/js/runtime-main.03334043.js"
+  },
+  {
+    "revision": "dab3a8e2e230af77bd84168f79c51cb7",
+    "url": "/portfolio/static/media/EstebanMendezResume.dab3a8e2.pdf"
+  }
+]);
